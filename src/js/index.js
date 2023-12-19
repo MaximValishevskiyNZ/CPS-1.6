20ec18d1a08d06abf24577ff40d4e89c7c615015
Maxim Valishevskiy <mvalishevskiy@only.com.ru>
2023-12-20 04:07:56 +0700

    fixes 1

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,9 @@ import { addSwiper } from './swiper'
 import { auto } from 'html-webpack-plugin/lib/chunksorter'
 import { showMore } from './showMore'
 
-
-
-
-
 const navigation = document.querySelector(".navigation");
-const burger = document.querySelector('.circle-button--bg-img--menu')
-const cross = document.querySelector('.circle-button--bg-img--cross')
+const burger = document.querySelector('.circle-button--menu')
+const cross = document.querySelector('.circle-button--cross')
 const body = document.querySelector("body");
 const articleShowMore = document.querySelector(".article__show-more-button");
 const articleText = document.querySelector(".article__text");
@@ -19,20 +15,21 @@ const brandsWrapper = document.querySelector(".brands__wrapper");
 const wrapper = document.querySelector(".wrapper");
 const repairsShowMoreButton = document.querySelector(".repairs__show-more-button");
 const repairsWrapper = document.querySelector(".repairs__wrapper");
-const repairsSwiper = document.querySelector(".repairs__swiper");
 const feedbackMessage = document.querySelector(".modal--feedback--message");
-const feedbackMessageOpen = document.querySelector(".circle-button--bg-img--message");
+const feedbackMessageOpen = document.querySelector(".circle-button--message");
 const feedbackMessageOpenNav = document.querySelector(".navigation__message-button");
 const feedbackMessageCross = document.querySelector(".modal--feedback--message .circle-button");
 const feedbackPhone = document.querySelector(".modal--feedback--phone");
-const feedbackPhoneOpen = document.querySelector(".circle-button--bg-img--phone");
+const feedbackPhoneOpen = document.querySelector(".circle-button--phone");
 const feedbackPhoneOpenNav = document.querySelector(".navigation__phone-button");
 const feedbackPhoneCross = document.querySelector(".modal--feedback--phone .circle-button");
 
 
 toggleAsideActive(burger, navigation, wrapper, body);
 toggleAsideActive(cross, navigation, wrapper, body);
-toggleAsideActiveOnOpen(navigation, burger, cross, wrapper, body);
+toggleAsideActiveOnOpen(feedbackMessage, 'circle-button--message', feedbackMessageCross, wrapper, 'modal--active' , navigation);
+toggleAsideActiveOnOpen(feedbackPhone, 'circle-button--phone', feedbackPhoneCross, wrapper, 'modal--active' , navigation);
+toggleAsideActiveOnOpen(navigation, 'circle-button--menu', cross, wrapper, 'navigation--active');
 toggleAsideActive(feedbackMessageOpen, feedbackMessage, wrapper, body, navigation);
 toggleAsideActive(feedbackMessageOpenNav, feedbackMessage, wrapper, body, navigation);
 toggleAsideActive(feedbackMessageCross, feedbackMessage, wrapper, body, navigation);
